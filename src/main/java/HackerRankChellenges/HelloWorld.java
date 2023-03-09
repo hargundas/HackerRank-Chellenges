9aa6a9950179aa95522ba9fba2903079b5e01f72
Hargun Das <hargun.das@paysyslabs.com>
2023-03-09 11:56:54 +0500

    first commit hello world program

---
+++ src/main/java/HackerRankChellenges/HelloWorld.java
@@ -0,0 +1,17 @@
+package HackerRankChellenges;
+
+import java.util.Scanner;
+
+/*
+  Hacker Rank Chellenge : 1
+ */
+public class HelloWorld {
+
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        String inputString =  sc.nextLine();
+        sc.close();
+        System.out.println(inputString);
+
+    }
+}
---
+++ src/test/java/com/mycompany/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
